d3743c2eab058ffbde5e0d80a58345bed3686a32
Ilya Hancharyk <Ilya_Hancharyk@epam.com>
2018-04-20 17:15:40 +0300

    added App component's render test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Index from './components/App/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Index />, div);
+it("Renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
